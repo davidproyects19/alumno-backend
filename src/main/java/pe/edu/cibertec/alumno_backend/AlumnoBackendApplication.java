d2bc3a27d2ade88f71a635f350d0344ca82ade65
DAVID <fernandez.davidgoza@gmail.com>
2024-10-22 18:55:46 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/alumno_backend/AlumnoBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.alumno_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/alumno_backend/AlumnoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.alumno_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
